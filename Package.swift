bd3d04014ebd026a529fb02469df06fb616a24d8
Elshad Yarmetov <168644604+elshadpb@users.noreply.github.com>
2024-10-02 13:20:15 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["InstanaAgent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elshadpb/GzipSwift.git", .branch("main"))
+        .package(url: "https://github.com/1024jp/GzipSwift.git", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
